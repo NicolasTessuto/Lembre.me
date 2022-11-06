454041be505bb7fafb7d94524cf39f96901bab39
nicolastessuto <nicolastessuto@gmail.com>
2022-11-06 10:35:06 -0300

    Recomeçando o projeto lembre-me agora com SpringBoot e trocando idioma das classes e métodos para ingles.

---
+++ Lembre.me/src/test/java/com/nicolastessuto/Lembre/me/LembreMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolastessuto.Lembre.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LembreMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
